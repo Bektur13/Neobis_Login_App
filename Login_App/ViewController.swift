5d33f1da4ce8fd5d5c1a6f948ca78d8f925878fc
Бектур Дуйшембеков <bekturduishembekov@MacBook-Air-Bektur.local>
2024-01-04 21:47:07 -0800

    Added files

---
+++ Login_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login_App
+//
+//  Created by Бектур Дуйшембеков on 12/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
